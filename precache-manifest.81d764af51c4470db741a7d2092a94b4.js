63085462b7a37725a655e38387be28071fca43eb
Code Sandbox <hello@codesandbox.io>
2022-04-18 14:50:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.81d764af51c4470db741a7d2092a94b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fe8661aa7a2fc1749be6b2d0cdc02f",
+    "url": "https://babybunn.github.io/csb-plgopg/index.html"
+  },
+  {
+    "revision": "da20ebf86532c0cf99a3",
+    "url": "https://babybunn.github.io/csb-plgopg/static/js/2.1db31354.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://babybunn.github.io/csb-plgopg/static/js/2.1db31354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6a8208f9083124c91f",
+    "url": "https://babybunn.github.io/csb-plgopg/static/js/main.45867dc9.chunk.js"
+  },
+  {
+    "revision": "05615541da3d52ce50eb",
+    "url": "https://babybunn.github.io/csb-plgopg/static/js/runtime-main.47011169.js"
+  }
+]);
